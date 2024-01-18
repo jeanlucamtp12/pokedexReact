b60d0f01b5d4524faf47983fd3f84cf6b27f4d29
jeanlucamtp12 <51718707+jeanlucamtp12@users.noreply.github.com>
2024-01-18 10:55:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Pokedex from "./Pokedex.jsx";
 import '../css/Pokedex.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import InfoPokemon from "./infoPokemon.jsx";
+import InfoPokemon from "./InfoPokemon.jsx";
 
 function App() {
   return (
